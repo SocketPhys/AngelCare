dd0a39045be8d20904975bde2396343c7358c6e1
SocketPhys <SocketPhys@gmail.com>
2016-06-25 14:06:54 -0700

    added modules

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+var low = require('lowdb')
+const db = low('db.json')
+var request = require('request');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
